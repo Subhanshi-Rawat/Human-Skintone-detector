10acc44278d4e4bf6266c0ce607dcd70365cb102
subhanshi <subhanshirawat@gmail.com>
2021-08-10 15:00:45 +0300

    first commit

---
+++ Major_Project/skintone/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from numpy.lib.type_check import imag
+from . import views
+
+# Create your models here.
+from django.contrib.auth.models import User
+
+from django.db.models.base import Model
+from . import managers
+
+
+class Fileupload(models.Model):
+    fileupload = models.FileField()
+    file_id = models.CharField(max_length=20,primary_key=True)
+    objects=managers.fileuploaddetails_manager()
+
